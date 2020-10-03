8d5657c14006a0e60c03533e927092f9aa998dce
Urban-Programmer <54013870+Urban-Programmer@users.noreply.github.com>
2020-10-03 03:41:29 -0400

    Fixed break protocol, Updated Wording,

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,47 +11,53 @@ namespace Calculator
         static void Main(string[] args)
         {
             /* This Program allows to the user to perform simple integer calculations */
+            /* This Version includes a loop */
 
-            Console.WriteLine("/**** Simple Calculator ****/ ");
+            Console.WriteLine("/**** Simple Looping Calculator ****/ ");
 
-            Console.WriteLine("Enter Number 1: ");
-            int number1 = Convert.ToInt32(Console.ReadLine());
+            while (true) {
 
-            Console.WriteLine("Enter Number 2: ");
-            int number2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Press 1 to Add, 2 to Subtract, 3 to Mulitply, 4 to Divide");
+                Console.WriteLine("Your Choice: ");
+                string userChoice = Console.ReadLine();
 
-            Console.WriteLine("Press 1 to Add, 2 to Subtract, 3 to Mulitply, 4 to Subtract");
+                Console.WriteLine("Enter Number 1: ");
+                int number1 = Convert.ToInt32(Console.ReadLine());
 
-            Boolean menuContinue = true; 
-            while (menuContinue) {
-                if (Console.ReadLine() == "1")
+                Console.WriteLine("Enter Number 2: ");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                if (userChoice == "1")
                 {
                     Console.WriteLine(number1 + " + " + number2 + " = " + (number1 + number2));
-                    break;
                 }
-                else if (Console.ReadLine() == "2")
+                else if (userChoice == "2")
                 {
                     Console.WriteLine(number1 + " - " + number2 + " = " + (number1 - number2));
-                    break;
                 }
-                else if (Console.ReadLine() == "3")
+                else if (userChoice == "3")
                 {
                     Console.WriteLine(number1 + " * " + number2 + " = " + (number1 * number2));
-                    break;
                 }
-                else if (Console.ReadLine() == "4")
+                else if (userChoice == "4")
                 {
                     Console.WriteLine(number1 + " / " + number2 + " = " + (number1 / number2));
-                    break;
                 }
                 else
                 {
                     Console.WriteLine("Not a valid choice");
-                    break;
-                }       
+                }
+
+                Console.WriteLine("Press n to quit, otherwise press any key to continue.");
+                if (Console.ReadLine().ToLower() == "n")
+                {
+                   break;
+                }
+
+
             }
-            menuContinue = false;
-            Console.ReadLine();
+
+
 
 
 
